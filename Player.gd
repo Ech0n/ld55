75354758bdf5bfa868c0fcd57a3b5e4e662635cf
Mateusz <matid@spoko.pl>
2024-04-13 15:55:01 +0200

    Added player attack animations

--- Player.gd
+++ Player.gd
@@ -9,22 +9,38 @@ const SPEED = 300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	animSprite.play("idle")	
 
+func update_sprite_animation(xVelocity):
+	if animSprite.animation == "attack":
+		return
+		
+	animSprite.play("walk")	
+	if xVelocity <0:
+		animSprite.flip_h = true
+	if xVelocity >0:
+		animSprite.flip_h = false
 
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Vector2( Input.get_axis("left", "right"), Input.get_axis("up","down"))
+	
 	velocity = direction.normalized() * SPEED
 	
-	if velocity and animSprite:
-		animSprite.play("walk")
-		if velocity.x <0:
-			animSprite.flip_h = true
-		if velocity.x >0:
-			animSprite.flip_h = false
-	else:
+	if velocity and animSprite:		
+		update_sprite_animation(velocity.x)
+	elif animSprite.animation != "attack":
 		animSprite.play("idle")
+		
+	var attack = Input.is_action_just_released("attack")
+	if(attack):
+		animSprite.play("attack")
 
 	move_and_slide()
+
+
+func _on_animation_finished():
+	animSprite.play("idle")
