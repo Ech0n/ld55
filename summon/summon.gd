222e1ed96406cb0e27477c74de40f1096faf582d
Mateusz <matid@spoko.pl>
2024-04-13 17:55:43 +0200

    Added following summon

---
+++ summon/summon.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export
+var SPEED : float = 500.0
+
+@export
+var player : Node2D
+
+func _physics_process(delta):
+		
+	global_transform.origin = lerp(global_transform.origin, player.position, delta)
+	
