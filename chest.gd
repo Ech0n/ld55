48511f8177abae43236571ff49c7286f1d9dd630
PilchS <stanislawpilch2002@gmail.com>
2024-04-13 20:36:41 +0200

    popups (pain)

--- chest.gd
+++ chest.gd
@@ -2,14 +2,29 @@ extends StaticBody2D
 
 var isPaused = false
 var allItems = ["armor", "book1", "book2", "book3", "dumbell", "milk", "speed", "blue_pot", "red_pot"]
-var item1
-var item2 
+var itemPopups = {}
 
 func _ready():
-	pass
+	for item in allItems:
+		var popup = Popup.new()
+		popup.add_child(Label.new())
+		popup.get_child(0).text = item
+		popup.hide()
+		itemPopups[item] = popup
+		add_child(popup)
 
-func pickup():
+func random_items():
 	print("Chest detected")
+	var item1 = allItems.pick_random()
+	var item2 = allItems.pick_random()
+	while item1 == item2:
+		item2 = allItems.pick_random()
+	print(item1 + " " + item2)
+	itemPopups[item1].show()
+	print("popup1")
+	itemPopups[item2].show()
+	print("popup2")
+	#pause_game()
 
 func pause_game():
 	isPaused = true
@@ -18,11 +33,9 @@ func pause_game():
 func resume_game():
 	isPaused = false
 	get_tree().paused = false
-	
-func random_items():
-	item1 = allItems.pick_random()
-	item2 = allItems.pick_random()
-	while item1 == item2:
-		item2 = allItems.pick_random()
-	print(item1 + " " + item2)
-		
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		resume_game()
+	for item in itemPopups.keys():
+		itemPopups[item].hide()
