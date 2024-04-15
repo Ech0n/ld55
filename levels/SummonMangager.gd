9d50a4bb746574f96e5c7f8145fb124187fb2515
Mateusz <matid@spoko.pl>
2024-04-15 17:05:40 +0200

    Added summoning circle spawning on summon

--- levels/SummonMangager.gd
+++ levels/SummonMangager.gd
@@ -2,14 +2,21 @@ extends Node2D
 
 var summoningCircleTexture = load("res://assets/summoning_ring.png")
 
+@onready
+var player = get_tree().get_nodes_in_group("player")[0]
+
 var summons : Array[Node2D]
 var i = 0
 
 func summon(ally):
 	print("recieved ally ")
-	if i < 3:
-		
+	if i <= 3:
 		summons.push_back(ally)
+		ally.position = player.position
 		add_child(ally)
+		var sumonCircle = Sprite2D.new()
+		sumonCircle.texture = summoningCircleTexture
+		sumonCircle.position = player.position
+		add_child(sumonCircle)
 		i+=1
 		
